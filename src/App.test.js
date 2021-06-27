040bd822fa81d2fd6b96dd35b891ee26d901a68e
Roland Levy <rolandjlevy@gmail.com>
2021-06-27 22:59:05 +0100

    Mocck test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
+import axios from 'axios';
 
 describe('Should render page elements and mock API calls', () => {
-  test('renders learn react link', () => {
+  test('renders learn react link', async () => {
     render(<App />);
 
     const linkElement = screen.getByText(/learn react/i);
@@ -13,6 +14,22 @@ describe('Should render page elements and mock API calls', () => {
     const btn = screen.getByRole('button', { name: 'Toggle'});
     userEvent.click( btn );
     screen.getByRole('heading', { name: '🚀'});
+
+    // Mock test
+    const axiosMock = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: {
+      "id":7,
+      "name":"Emily Levy",
+      "username":"EJLevy",
+      "email":"ejlevy@gmail.com",
+    }});
+
+    userEvent.click(
+      screen.getByRole('button', { name: 'Get user'})
+    )
+
+    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(1));
+
+    screen.logTestingPlaygroundURL();
   
     // test 'Learn React' link
     screen.getByRole('link', { name: 'Learn React'});
